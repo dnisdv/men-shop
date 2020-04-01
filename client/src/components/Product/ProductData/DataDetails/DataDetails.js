9c6c66043a03bfe59470ee43f198e87eacd052c5
dnisdv <iamdenisgradinaru@gmail.com>
2020-04-01 15:39:55 +0300

    added DataDetails component

--- client/src/components/Product/ProductData/DataDetails/DataDetails.js
+++ client/src/components/Product/ProductData/DataDetails/DataDetails.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './DataDetails.css'
 
 const DataDetails = () => {
     return(
@@ -6,11 +7,16 @@ const DataDetails = () => {
             <h2 className='DataDetails_Title'>DETAILS & CARE</h2>
 
             <div className='DataDetails_Data'>
-                <p className='DataDetails_Data'>Modern understatement and handcrafted quality come together 
+                <p className='DataDetails_Data_Main'>Modern understatement and handcrafted quality come together 
                 in a full-grain leather wallet with vibrant stitch detailing. The smart style offers Tumi 
                 ID Lock technology to help protect personal data encoded on your cards from electronic theft. 
                 A ballistic nylon spine ensures durability and longevity.</p>
-
+                <ol className='DataDetails_Data_List'>
+                    <li className='DataDetails_Data_List_Item'>100% Guarantee</li>
+                    <li className='DataDetails_Data_List_Item'>Fast Shipping</li>
+                    <li className='DataDetails_Data_List_Item'>Money Return</li>
+                    <li className='DataDetails_Data_List_Item'>Free Delivery</li>
+                </ol>
             </div>
         </div>
     )
--- client/src/components/Product/ProductData/ProductData.js
+++ client/src/components/Product/ProductData/ProductData.js
@@ -13,13 +13,20 @@ const CharcsData = [
 
 const ProductData = () => {
     return(
-        <div className='ProductData'>
-            {/* <h2 className='ProductData_Title'>Modern understatement and handcrafted quality come together
-                                              in a full-grain leatherwallet with vibrant stitch detailing</h2> */}
-            {/* <DataDetails /> */}
-            
-            <DataCharcs options={CharcsData} />
-        </div>
+        <React.Fragment>
+            <div className='ProductData'>
+                <h2 className='ProductData_Title'>Modern understatement and handcrafted quality come together
+                                                in a full-grain leatherwallet with vibrant stitch detailing</h2>
+            </div>
+
+                <img src='https://cutt.ly/ItYAf4v' className='ProductData_MainImage' />
+
+            <div className='ProductData ProductData-Second'>
+                <DataDetails />
+                
+                <DataCharcs options={CharcsData} />
+            </div>
+            </React.Fragment>
     )
 }
 
