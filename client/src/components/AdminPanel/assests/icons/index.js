a9751fb12bf30517dbf5fa3b4aee0ae1bf023d4c
dnisdv <iamdenisgradinaru@gmail.com>
2020-08-29 16:32:45 +0300

    added server

--- client/src/components/AdminPanel/assests/icons/index.js
+++
@@ -1,7 +0,0 @@
-export {default as cartIcon} from './cart.svg';
-export {default as categoryIcon} from './category.svg';
-export {default as productsIcon} from './products.svg';
-export {default as reviewsIcon} from './reviews.svg';
-export {default as usersIcon} from './users.svg';
-export {default as bannerIcon} from './banner.svg';
-
