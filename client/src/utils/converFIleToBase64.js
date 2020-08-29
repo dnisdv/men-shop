8137792cfb5f2074dd4e3a35e12fe61624414770
dnisdv <iamdenisgradinaru@gmail.com>
2020-08-29 17:03:14 +0300

    added server branch

--- client/src/utils/converFIleToBase64.js
+++
@@ -1,9 +0,0 @@
-export default file =>{
-
-return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
-
-    reader.readAsDataURL(file.rawFile);
-})}
