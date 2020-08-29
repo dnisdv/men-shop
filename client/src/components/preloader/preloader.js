202e562fe34712e8e82fdd49c74cf11b35f95c5e
dnisdv <iamdenisgradinaru@gmail.com>
2020-08-29 17:53:26 +0300

    asfwa

--- client/src/components/preloader/preloader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './preloader.css'
-
-export default () =>(
-    <div className="cssload-container">
-        <div className="cssload-speeding-wheel"></div>
-    </div>
-)
